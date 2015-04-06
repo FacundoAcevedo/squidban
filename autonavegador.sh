b3cdadd5aef3ed868f1b0d2b76fd8e9d68c2cb30
Facundo Acevedo <facevedo@openmailbox.org>
2015-04-06 13:01:44 -0300

    Random ip

--- autonavegador.sh
+++ autonavegador.sh
@@ -9,15 +9,14 @@
 
 #Generador magico de navegacion
 
-tiempoFuera=2
 
 rutaAccessLog="/var/log/squid/access.log"
 
 while [ 1 ] ; do
-    str="$(date +%s).000 958 5.0.2.20 TCP_MISS/200 2252 CONNECT xmlrpc.rhn.redhat.com:443 - HIER_DIRECT/209.132.183.44 -"
+
+    str=$(date +%s)'.000 958 5.0.2.'${RANDOM: -2}' TCP_MISS/200 2252 CONNECT xmlrpc.rhn.redhat.com:443 - HIER_DIRECT/209.132.183.44 -'
     echo "$str" >> "$rutaAccessLog"
     echo "$str"
-    sleep "$tiempoFuera"
 done
 
 
